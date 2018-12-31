7340346bb5a9a4d6c930bad0f181c82bd6fd323e
Milo <thoofadhli1987@gmail.com>
2018-12-31 23:59:37 +0800

    added a new feature some files changed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'User',
-            'email' => 'user1@email.com',
-            'password' => bcrypt('ManhajAtTulaab123'),
-        ]);
-        DB::table('users')->insert([
-            'name' => 'user2',
-            'email' => 'user2@email.com',
-            'password' => bcrypt('ManhajAtTulaab123'),
-        ]);//// $this->call(UsersTableSeeder::class);
+        //// $this->call(UsersTableSeeder::class);
     }
 }
